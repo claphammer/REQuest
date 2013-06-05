c0a1ea0ab66a4653d86a18cdc3663f5ae9bc41ba
Nathan Josway <NJosway@nathan-josways-mac-pro.local>
2013-06-05 16:14:11 -0700

    Revised Terrain and menu system

--- Assets/REQuest Assets/Scripts/Common/MenuSystem.js
+++ Assets/REQuest Assets/Scripts/Common/MenuSystem.js
@@ -2,7 +2,9 @@
 
 function OnGUI () {
 	
-	GUI.Box(Rect(0,0,768,1024),"REQuest Demo Levels");
+	GUI.Box(Rect(0,0,768,1024),"");
+	
+	GUI.Label(Rect(340,20,100, 20),"REQuest Demo Levels");
 	
 	if(GUI.Button(Rect(255,50,250,100),"Level 1"))
 		Application.LoadLevel("REQuestDemo02");
